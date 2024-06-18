6ea88a70eb151c3dddd25f0f80c7671ebf86fb96
AndreaGepponi <127541900+AndreaGepponi@users.noreply.github.com>
2024-06-18 11:53:17 +0200

    Update BookingMapper.java

--- Test_SWE/src/DomainModel/BookingMapper.java
+++ Test_SWE/src/DomainModel/BookingMapper.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 
 public class BookingMapper {
 
-    public Booking map(String u_name, Calendar s, Calendar e, String h, int id){
+    public Booking map(Customer u_name, Calendar s, Calendar e, String h, int id){
         Booking booking = new Booking();
 
         booking.setH(h);
